aa972a87e28467ac4fc2ebea2f8fb2d09a736d84
Kashyap Vinodbhai Nasit <kashyap.nasit@myntra.com>
2023-06-06 23:06:43 +0530

    Added comment

--- src/main/scala/com/nasit/ColumnLineageExample.scala
+++ src/main/scala/com/nasit/ColumnLineageExample.scala
@@ -4,29 +4,43 @@ import org.apache.spark.sql.SparkSession
 
 object ColumnLineageExample {
   def main(args: Array[String]): Unit = {
+
+    //Create Spark Session.
     val spark = SparkSession.builder()
       .appName("ColumnLineageExample")
       .master("local[*]")
       .getOrCreate()
 
+    //Read order detail CSV file.
     val orderDetailDf = spark.read
       .format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load("src/main/resources/order_details.csv")
+
+    //Put it in cache.
     orderDetailDf.cache()
+
+    //Create a view from it.
     orderDetailDf.createOrReplaceTempView("order_detail")
 
+    //Read from user_info CSV file.
     val userDetailDf = spark.read
       .format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load("src/main/resources/user_info.csv")
+
+    //Put it in cache.
     userDetailDf.cache()
+
+    //Create a view from it.
     userDetailDf.createOrReplaceTempView("user_info")
 
+    //We are registering a listener. This can be done via `--conf "spark.extraListeners=com.package.ListenerClass"` as well while submitting spark application
     spark.listenerManager.register(new QueryListener)
 
+    // A sample query which joins both the data sources and gives avg order value with full name of customer.
     val sqlQuery =
       """
         |SELECT
@@ -55,6 +69,10 @@ object ColumnLineageExample {
 
     val queryResult = spark.sql(sqlQuery)
     queryResult.show(false)
+
+    orderDetailDf.unpersist()
+    userDetailDf.unpersist()
+
     spark.stop()
   }
 }
